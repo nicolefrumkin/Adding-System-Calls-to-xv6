31125fc48ccfc2b2ebcd850f32abb3e6404cfcf0
nicolefrumkin <nicolefrumkin@gmail.com>
2025-01-11 11:08:30 +0200

    starting point

---
+++ ps.c
@@ -0,0 +1,6 @@
+int main() {
+    printf("Total number of active processes: %d\n", active_processes);
+    printf("Maximum PID: %d\n", max_pid);
+    printf("PID    STATE    PPID    SZ    NFD    NRSWITCH");
+    return 0;
+}
